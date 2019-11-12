5214f181c1857f6b0e2ac43a01b7d9a33b3de22d
Loukas Litsos <lkslts64@gmail.com>
2019-11-13 00:51:45 +0200

    Add DTW feature: return index pairs of optimal traversal
    I added another method for taking the index pairs for one of the
    possible optimal traversals. We 'll need this when clustering curves.

---
+++ src/distance.cpp
@@ -0,0 +1,114 @@
+#include <math.h>
+#include <cfloat>
+#include "distance.h"
+#include "LSH.h"
+
+double Manhattan::dist(Object *obj1,Object *obj2){
+    auto p1 = dynamic_cast<Point *>(obj1);
+    auto p2 = dynamic_cast<Point *>(obj2);
+    if (p1->getCoordinates().size() != p2->getCoordinates().size())
+        return -1;
+    double sum = 0;
+    int sz = p1->getCoordinates().size();
+    for(int i =0; i < sz;i++)
+        sum += fabs(p1->getCoordinate(i) - p2->getCoordinate(i));
+    return sum;
+}
+
+double euclidean(Point p1,Point p2) {
+    if (p1.getCoordinates().size() != p2.getCoordinates().size())
+        return -1;
+    double dist;
+    double sum = 0.0;
+    for (int i=0; i < p1.getCoordinates().size(); i++) {
+        dist = p1.getCoordinate(i) - p2.getCoordinate(i);
+        sum += dist*dist;
+    }
+    return sqrt(sum);
+}
+
+
+int DTW::min(double left,double up,double diagonal,int *succesor) {
+    if (left <= up && left <= diagonal) {
+        *succesor = this->left;
+        return left;
+    }
+    else if (up <= left && up <= diagonal) {
+        *succesor = this->up;
+        return up;
+    }
+    else {
+        *succesor = this->diagonal;
+        return diagonal;
+    }
+}
+
+void DTW::backtrack(vector<struct IndexPairs> *ipairs) {
+    //auto ipairs = new vector<struct IndexPairs>();
+    struct IndexPairs ipair;
+    auto i = c1Len-1;
+    auto j = c2Len-1;
+    //push last pair
+    ipair = {c1Len-1,c2Len-1};
+    ipairs->push_back(ipair);
+    while (arr[i][j].predecessor != this->start) {
+        switch (arr[i][j].predecessor) {
+        case this->left:
+            j--;
+            break;
+        case this->up:
+            i--;
+            break;
+        case this->diagonal:
+            i--;
+            j--;
+            break;
+        }
+        ipair = {i,j};
+        ipairs->insert(ipairs->begin(),ipair);
+    }
+}
+
+
+double DTW::dist(Object *obj1,Object *obj2){
+    auto c1 = dynamic_cast<Curve *>(obj1);
+    auto c2 = dynamic_cast<Curve *>(obj2);
+    c1Len = c1->getPoints().size();
+    c2Len = c2->getPoints().size();
+    int predecessor;
+    arr = new struct DTWTuple*[c1Len];
+    for (int i =0; i < c1Len; i++)
+        arr[i] = new struct DTWTuple[c2Len];
+    for (int i =0; i < c1Len; i++) {
+        for (int j =0; j < c2Len; j++) {
+            if (!i && !j) {//both zero
+                arr[i][j].dist = euclidean(c1->getPoint(i),c2->getPoint(j));
+                arr[i][j].predecessor = this->start;
+            }
+            else if (!i) {
+                arr[i][j].dist = arr[i][j-1].dist + euclidean(c1->getPoint(i),c2->getPoint(j));
+                arr[i][j].predecessor = this->left;
+            }
+            else if (!j) {
+                arr[i][j].dist = arr[i-1][j].dist + euclidean(c1->getPoint(i),c2->getPoint(j));
+                arr[i][j].predecessor = this->up;
+            }
+            else {
+                arr[i][j].dist = this->min(arr[i][j-1].dist,arr[i-1][j].dist,arr[i-1][j-1].dist,&predecessor) +
+                euclidean(c1->getPoint(i),c2->getPoint(j));
+                arr[i][j].predecessor = predecessor;
+            }
+        }
+    }
+    return arr[c1Len-1][c2Len-1].dist;
+}
+
+double DTW::distWithIndexPairs(Object *obj1,Object *obj2,vector<struct IndexPairs> *indexPairs) {
+    double dist = this->dist(obj1,obj2);
+    backtrack(indexPairs);
+    return dist;
+}
+
+DTW::~DTW() {
+    delete []arr;
+}
---
+++ src/distance.h
@@ -0,0 +1,40 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+#include "Point.h"
+
+class DistanceMetric {
+    public:
+    virtual double dist(Object* obj1,Object *obj2) = 0;
+};
+
+class Manhattan : public DistanceMetric {
+    public:
+    double dist(Object *obj1,Object *obj2);
+};
+
+struct DTWTuple {
+    int predecessor;
+    double dist;
+}; 
+
+struct IndexPairs {
+    int p1;
+    int p2;
+};
+
+
+class DTW : public DistanceMetric {
+    enum Predecessor {left,up,diagonal,start};
+    struct DTWTuple **arr;
+    int c1Len;
+    int c2Len;
+    //returns min of the first three values and sets predecessor 
+    int min(double left,double up,double diagonal,int *predecessor);
+    public:
+    double distWithIndexPairs(Object *obj1,Object *obj2,vector<struct IndexPairs> *indexPairs);
+    double dist(Object *obj1,Object *obj2);
+    void backtrack(vector<struct IndexPairs> *ipairs);
+    ~DTW();
+};
+
+#endif
