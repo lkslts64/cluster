eca987ff21c752688518516dbd391697237064d0
Loukas Litsos <lkslts64@gmail.com>
2019-11-13 23:00:18 +0200

    Merge

--- src/ui.h
+++ src/ui.h
@@ -1,5 +1,6 @@
 #ifndef ALGORITHMS_PROJECT_UI_H
 #define ALGORITHMS_PROJECT_UI_H
+#include "LSH.h"
 
 void readArgumentsLSHPoints(LSH* lsh, int argc, char **argv);
 void readArgumentsLSHCurves(LSH* lsh, int argc, char **argv);
