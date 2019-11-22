815955d71834855cdd320b354dd9d21d587e3137
Loukas Litsos <lkslts64@gmail.com>
2019-11-22 16:46:59 +0200

    Add enums representing algorithms/strategies

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,15 @@
 
 using namespace std;
 
+enum InitAlgo {Random,SpreadOut};
+enum AssignAlgo {Lloyd,Inverse};
+enum UpdateAlgo {PAM,Centroid};
 InitStrategy* init;
 AssignmentStrategy* assign;
 UpdateStrategy* update;
 
-void executeAlgorithm(Cluster *,int,int,int);
+void executeAlgorithm(Cluster* cluster, InitAlgo init_selection, AssignAlgo assign_selection,
+ UpdateAlgo update_selection);
 
 int main(int argc, char* argv[]) {
     auto cluster = new Cluster();
@@ -20,30 +24,33 @@ int main(int argc, char* argv[]) {
     cluster->setData(parseFile(cluster->getGeneralParameters()->getInputFilename()));
     //test_print_data(cluster->getDataset());
 
-    executeAlgorithm(cluster,1,1,1);
-    executeAlgorithm(cluster,1,1,2);
-    executeAlgorithm(cluster,1,2,1);
-    executeAlgorithm(cluster,1,2,2);
-    executeAlgorithm(cluster,2,1,1);
-    executeAlgorithm(cluster,2,1,2);
-    executeAlgorithm(cluster,2,2,1);
-    executeAlgorithm(cluster,2,2,2);
+    executeAlgorithm(cluster,Random,Lloyd,PAM);
+    executeAlgorithm(cluster,Random,Lloyd,Centroid);
+    executeAlgorithm(cluster,Random,Inverse,PAM);
+    executeAlgorithm(cluster,Random,Inverse,Centroid);
+    executeAlgorithm(cluster,SpreadOut,Lloyd,PAM);
+    executeAlgorithm(cluster,SpreadOut,Lloyd,Centroid);
+    executeAlgorithm(cluster,SpreadOut,Inverse,PAM);
+    executeAlgorithm(cluster,SpreadOut,Inverse,Centroid);
 }
 
-void executeAlgorithm(Cluster* cluster, int init_selection, int assign_selection, int update_selection){
-    if(init_selection == 1)   init = new RandomInit(cluster);
-    else                      init = new SpreadOutInit(cluster);
-    if(assign_selection == 1) assign = new LloydAssignment(cluster);
-    else                      assign = new InverseAssignment(cluster);
-    if(update_selection == 1) update = new PAMUpdate(cluster);
-    else                      update = new CentroidUpdate(cluster);
-
-    /**
-     * Start Timer
-     */
+void executeAlgorithm(Cluster* cluster, InitAlgo init_selection, AssignAlgo assign_selection,
+ UpdateAlgo update_selection){
+     switch (init_selection) {
+        case Random:    init = new RandomInit(cluster);break;
+        case SpreadOut: init = new SpreadOutInit(cluster);break;
+     }
+     switch (assign_selection) {
+        case Lloyd:   assign = new LloydAssignment(cluster);break;
+        case Inverse: assign = new InverseAssignment(cluster);break;
+     }
+     switch (update_selection) {
+        case PAM:      update = new PAMUpdate(cluster);break;
+        case Centroid: update = new CentroidUpdate(cluster);break;
+     }
     auto start = chrono::steady_clock::now();
-
-    cout <<"============="<<endl<< "ALGORITHM " << init_selection << assign_selection << update_selection << endl<<"============="<<endl;
+    cout <<"============="<<endl<< "ALGORITHM " << init_selection+1 << assign_selection+1 << 
+    update_selection+1 << endl<<"============="<<endl;
     cout << "initializing..." << endl;
     init->execute();
     int count = 0;
@@ -55,24 +62,18 @@ void executeAlgorithm(Cluster* cluster, int init_selection, int assign_selection
             cout << "update break!" << endl;
             break;
         }
-    }while(count++ < 2);
+    }while(count++ < 50);
     //a last assignment is mandatory.
     //otherwise clusters map has no values
     cout << count << ". assigning..." << endl;
     assign->execute();
     cout << "finished clustering!" << endl;
-
-    /**
-     * End Timer
-     */
     auto end = chrono::steady_clock::now();
     auto time_span = static_cast<chrono::duration<double>>(end - start);
     cout<<"Operation took: "<< time_span.count() <<" seconds" << endl;
-
     stringstream firstLine;
-    firstLine << "Algorithm I" << init_selection << "A" << assign_selection << "U" << update_selection;
+    firstLine << "Algorithm I" << init_selection+1 << "A" << assign_selection+1 << "U" << update_selection+1;
     cluster->output(firstLine.str(), time_span.count());
-
     //clean
     cluster->clear();
     delete init;
